7aeb18c72afb38bd1ede031912a9d0f22d729de6
Tonyreng <tonyreng01@gmail.com>
2025-04-16 03:57:12 +0000

    Dando forma y posición a la navbar

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -10,9 +10,10 @@ import '../styles/index.css'
 
 // components
 import Home from './components/Home';
+import Navbar from './components/navbar';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home/>
+    <Navbar />
   </React.StrictMode>,
 )
